c0b83f53d99c72b68b3206cc7501902074d31348
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-03-25 18:19:57 +0800

    Update 224_1.cpp

--- 224_1.cpp
+++ 224_1.cpp
@@ -1,16 +1,29 @@
 #include<stdio.h>
 int main()
 {
-	int A[100000]={},a,b=0,c,d,e,B[1000]={};
+	int A[100000]={},a,b=0,c,d,e,B[1000]={},C[10000]={};
 	A[0]=1;
 	//Cube Free Number ??
 	for(a=1;a<10000;a++)
 	{
-		B[b]=a*a*a;
+	B[a]=a*a*a;
+		for(c=0;c<1000000;c++)
+		{
+		
 		b++;
 		printf("%d\n",B[b]);
+		}
 	}
-	
+	for(a=1;a<10000;a++)
+	{
+	B[a]=a*a*a;
+		for(c=0;c<1000000;c++)
+		{
+		
+		b++;
+		printf("%d\n",B[b]);
+		}
+	}
 	
 	
 	
